a50f37f5f91c38f8afe826b74ccff0e9bb51f071
Carolyn <carol.nguyen2007@gmail.com>
2022-10-13 17:16:18 -0700

    hi

--- q2.cpp
+++ q2.cpp
@@ -9,6 +9,13 @@ void printResult(int &, int &, int &, int);
 // Your code here
 // ******************************
 
+int main()
+{
+	getInput(n1, n2, n3);
+	int min = findMin(n1, n2, n3);
+	printResult(n1, n2, n3, min);
+}
+
 void getInput(int &n1, int &n2, int &n3)
 {
 	cout << "Enter in three values: ";
@@ -17,8 +24,8 @@ void getInput(int &n1, int &n2, int &n3)
 
 int findMin(int n1, int n2, int n3)
 {
-	int min;
-	int nums[3];
+	int min, nums[3];
+	nums[0] = n1, nums[1] = n2, nums[2] = n3;
 	min = nums[0];
 	for (int i = 1; i < 3; i++)
 	{
